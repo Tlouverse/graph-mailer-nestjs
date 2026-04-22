8a0baedaaad06c9e5ced70c86c9418a629170481
Louvet Thomas <thomas.louvet@laposte.net>
2026-04-22 16:43:03 +0200

    replace unkown with any in the factory arg list

--- src/graph-mailer.interfaces.ts
+++ src/graph-mailer.interfaces.ts
@@ -2,6 +2,6 @@ import type { ModuleMetadata, Type } from '@nestjs/common';
 import type { GraphMailerConfig } from '@tlouverse/graph-mailer';
 
 export interface GraphMailerAsyncOptions extends Pick<ModuleMetadata, 'imports'> {
-  useFactory: (...args: unknown[]) => GraphMailerConfig | Promise<GraphMailerConfig>;
+  useFactory: (...args: any[]) => GraphMailerConfig | Promise<GraphMailerConfig>;
   inject?: (string | symbol | Type)[];
 }
